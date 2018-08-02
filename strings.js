b5809a009207d443978e78ed19b2b4e17cd8ce0b
CaitlinCodes <caiit@me.com>
2018-08-02 16:22:44 -0600

    ice cream challenge

--- strings.js
+++ strings.js
@@ -14,6 +14,8 @@
 
 var jsHistory = 'JavaScript, not to be confused with Java, was created in 10 days in May 1995 by Brendan Eich, then working at Netscape and now of Mozilla. JavaScript was not always known as JavaScript: the original name was Mocha, a name chosen by Marc Andreessen, founder of Netscape. In September of 1995 the name was changed to LiveScript, then in December of the same year, upon receiving a trademark license from Sun, the name JavaScript was adopted. This was somewhat of a marketing move at the time, with Java being very popular around then.';
 
+//charAt a possible aide?
+
 function countLetterA(str, char) {
 
   var i = 0;
@@ -23,7 +25,6 @@ function countLetterA(str, char) {
   }
   return countLetterA;
 }
-
 
 //3. Given the following sentence: Uppercase the entire word scream each time it appears in the sentence 
 //I SCREAM, you SCREAM, we all SCREAM for icecream
